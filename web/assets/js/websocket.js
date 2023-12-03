b0422cd22f61202aa9dafd14d2b366d80f3e3d95
BE767 <martinez.guzman.evelyn.briseth@gmail.com>
2023-12-03 09:07:57 -0600

    Cambios en el JavaScript

--- web/assets/js/websocket.js
+++ web/assets/js/websocket.js
@@ -1,6 +1,10 @@
-var wsURI = "ws://" + document.location.host + document.location.pathname + "chat";
+
+
+var serverIP = "192.168.100.20"; 
+var wsURI = "ws://" + serverIP + ":8080/WebSocketsBM/chat";
 
 var websocket = new WebSocket(wsURI);
+console.log("WebSocket readyState: " + websocket.readyState);
 
 websocket.onmessage = function(evnt) {
     onMessage(evnt);
@@ -9,7 +13,6 @@ websocket.onmessage = function(evnt) {
 websocket.onopen = function()  {
     onOpen();
 };
-
 function onOpen() {
     console.log("Opened connection: " + wsURI);
 }
@@ -18,25 +21,33 @@ function onClose() {
     console.log("Closed connection: " + wsURI);
 }
 
+websocket.onerror = function(event) {
+     
+    console.error("WebSocket error:", event.message);
+};
+
 function onMessage(event) {
     console.log(event);
     display(event.data);
 }
 
 function display(dataString) {
-   
     var data = JSON.parse(dataString);
-    var contentMessage = "<p>User " + data.userName + " says: " + data.conten + "</p>";
+    var contentMessage = "<p>Usuario " + data.userName + " envia: " + data.conten + "</p>";
     document.getElementById("output").innerHTML += contentMessage + "</br>";
 }
 
 function send() {
-    var message = document.getElementById("message_input").value;
-    var username = document.getElementById("username_input").value;
-    var json = {
-        "conten": message,
-        "userName": username
-    };
-    console.log("Sending " + message);
-    websocket.send(JSON.stringify(json));
+    if (websocket.readyState === WebSocket.OPEN) {
+        var message = document.getElementById("message_input").value;
+        var username = document.getElementById("username_input").value;
+        var json = {
+            "conten": message,
+            "userName": username
+        };
+        console.log("Sending " + message);
+        websocket.send(JSON.stringify(json));
+    } else {
+        console.error("WebSocket connection is not open.");
+    }
 }
